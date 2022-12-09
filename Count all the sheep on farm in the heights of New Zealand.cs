e662510bd71c7be3d566883fb5cedc92e104e136
Aryan Patil <84935470+aryanator@users.noreply.github.com>
2022-12-09 17:10:07 +0530

    Update Count all the sheep on farm in the heights of New Zealand.cs

--- Count all the sheep on farm in the heights of New Zealand.cs
+++ Count all the sheep on farm in the heights of New Zealand.cs
@@ -1,1 +1,13 @@
-
+using System;
+using System.Linq;
+namespace CodeWars
+{
+    public class Kata
+    {
+        public int lostSheep(int[] friday, int[] saturday, int total)
+        {
+            // Code here
+         return total - friday.Sum() - saturday.Sum();
+        }
+    }
+}
