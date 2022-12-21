832b3a2783af0b257be3cc3c6fa474356e993f15
Aryan Patil <84935470+aryanator@users.noreply.github.com>
2022-12-21 21:13:40 +0530

    Update Calculate average.py

--- Calculate average.py
+++ Calculate average.py
@@ -1,1 +1,10 @@
-
+using System;
+using System.Linq;
+class AverageSolution
+{
+  public static double FindAverage(double[] array)
+  {
+    if(array.Length==0) return 0;
+    else return array.Average();
+  }
+} 
