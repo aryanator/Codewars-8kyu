4fe58b34e34c5479fcea46c4e9d74c14b7b5fcd4
Aryan Patil <84935470+aryanator@users.noreply.github.com>
2022-12-27 07:35:40 +0530

    Update Grasshopper - Summation.cs

--- Grasshopper - Summation.cs
+++ Grasshopper - Summation.cs
@@ -1,1 +1,14 @@
+using System.Linq;
+using System;
 
+public static class Kata 
+{
+    public static int summation(int num)
+    {
+      int s=0;
+      for (int i=1;i<=num;i++){
+        s=s+i;  
+      }
+      return s;
+    }
+}
