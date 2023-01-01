a23d29ecce01240c82e15d97234edbe26e3e5e5e
Aryan Patil <84935470+aryanator@users.noreply.github.com>
2023-01-01 20:38:51 +0530

    Update Will you make it?.cs

--- Will you make it?.cs
+++ Will you make it?.cs
@@ -4,5 +4,14 @@ public static class Kata
 {
   public static bool ZeroFuel(uint distanceToPump, uint mpg, uint fuelLeft)
   {
+      if(distanceToPump <= mpg * fuelLeft){
+      return true;
+    }
+    else{
+      return false;
+    }
+    throw new NotImplementedException();
+  }
+}
     
   
