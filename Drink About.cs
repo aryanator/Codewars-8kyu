f7580091b4565683552775c8f2aacd92216b4609
Aryan Patil <84935470+aryanator@users.noreply.github.com>
2023-01-04 15:24:58 +0530

    Update Drink About.cs

--- Drink About.cs
+++ Drink About.cs
@@ -1,3 +1,10 @@
 public class Kata
 {
- 
+  public static string PeopleWithAgeDrink(int old)
+  {
+     if (old >= 21) return "drink whisky";
+            if (old >= 18) return "drink beer";
+            if (old >= 14) return "drink coke";
+            return "drink toddy";
+  }
+}
