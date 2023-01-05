20d18e8f1a3e671ff4f857a5456434009be55892
Aryan Patil <84935470+aryanator@users.noreply.github.com>
2023-01-05 22:52:32 +0530

    Update What is in in between.cs

--- What is in in between.cs
+++ What is in in between.cs
@@ -1,1 +1,12 @@
-
+using System.Collections.Generic;
+using System.Linq;
+public class Kata {
+  public static int[] Between(int a, int b) {
+             var result = new List<int>();
+            for (var i = a; i <= b; i++)
+            {
+                result.Add(i);
+            }
+            return result.ToArray();
+        }
+   }
