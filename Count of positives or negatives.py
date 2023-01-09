05bdf2d6caf4825aab99eee0fb7f7e7ca13e281d
Aryan Patil <84935470+aryanator@users.noreply.github.com>
2023-01-09 09:20:18 +0530

    Update Count of positives or negatives.py

--- Count of positives or negatives.py
+++ Count of positives or negatives.py
@@ -1,1 +1,12 @@
-
+def count_positives_sum_negatives(arr):
+    p=0
+    q=0
+    if arr==[]:
+        return []
+    else:
+        for i in arr:
+            if i>0:
+                p=p+1
+            if i<0:
+                q=q+i
+        return[p,q]
